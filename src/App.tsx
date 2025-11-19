f5181b0c44b0fcc540aca0c0cc099af86bdc3957
kelvinTech <kelvinnjuguna357@gmail.com>
2025-11-19 17:02:27 +0300

    final changes before deployment

--- src/App.tsx
+++ src/App.tsx
@@ -8,5 +8,5 @@ function App() {
     </div>
   );
 }
-
+//end
 export default App;
--- src/dashboard/home.tsx
+++ src/dashboard/home.tsx
@@ -204,7 +204,7 @@ function Home() {
         if (res.data.success && Array.isArray(res.data.posts)) {
           setPosts(res.data.posts);
         } else {
-          console.error("❌ Failed to fetch posts:", res.data.message);
+          console.error(" Failed to fetch posts:", res.data.message);
         }
         setActualUser(res.data);
         if (res2.data.success) {
@@ -212,7 +212,7 @@ function Home() {
           setLocalCaption(res.data.bio);
         }
       } catch (err) {
-        console.error("🔥 Error fetching posts:", err);
+        console.error(" Error fetching posts:", err);
       } finally {
         setLoading(false);
       }
